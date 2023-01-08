8e3873b0b68f3f59a22fec296d2e0e785ad5714f
richardbmezzomo <richardmezzomo1@gmail.com>
2023-01-07 22:18:35 -0300

    adds a function to change the minutes

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes;
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
@@ -32,4 +35,9 @@ buttonSoundOn.addEventListener('click', function () {
 buttonSoundOff.addEventListener('click', function () {
   buttonSoundOff.classList.add('hide')
   buttonSoundOn.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function () {
+  minutes = prompt('How many minutes?')
+  minutesDisplay.textContent = minutes 
 })

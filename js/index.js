32ec2092e423cf06b9ea0f33a73b7f6275a6b0bd
richardbmezzomo <richardmezzomo1@gmail.com>
2023-01-10 16:11:46 -0300

    adds some sounds

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ import Timer from "./timer.js"
 
 import { elements } from "./elements.js"
 
+import Sound from "./sounds.js"
+
 const {
   buttonPlay,
   buttonPause,
@@ -30,29 +32,36 @@ const timer = Timer({
     resetControls: controls.reset
 })
 
+const sound= Sound()
+
 buttonPlay.addEventListener('click', function () {
   controls.play()
   timer.countdown()
+  sound.pressButton()
 })
 
 buttonPause.addEventListener('click', function () {
   controls.pause()
   timer.hold()
+  sound.pressButton()
 })
 
 buttonStop.addEventListener('click', function () {
   controls.reset()
   timer.reset()
+  sound.pressButton()
 })
 
 buttonSoundOn.addEventListener('click', function () {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
+  sound.bgAudio.pause()
 })
 
 buttonSoundOff.addEventListener('click', function () {
   buttonSoundOff.classList.add('hide')
   buttonSoundOn.classList.remove('hide')
+  sound.bgAudio.play()
 })
 
 buttonSet.addEventListener('click', function () {
--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,7 @@
+import Sounds from "./sounds.js"
+
+
+
 export default function Timer({
   minutesDisplay,
   secondsDisplay,
@@ -31,6 +35,7 @@ export default function Timer({
       if(isFinished) {
         resetControls()
         updateDisplay()
+        Sounds().timeEnd()
         return
       }
 
